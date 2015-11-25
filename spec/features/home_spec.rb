375bd6df189dc351d557b5f79f130fbe01a2c617
donald-s <don@parablesoft.com>
2015-11-25 14:07:00 -0500

    Initial commit.

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'Home', js: true do
+	scenario 'Byebug does not hang' do
+		visit root_path
+
+		expect(page).to have_selector('h1', visible: true)
+		byebug # run find('h1') to reproduce the hang 
+	end
+end
